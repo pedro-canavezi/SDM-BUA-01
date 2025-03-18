05532e970a86586526615c2e9b600a2cdd5fc18e
pedro-canavezi <pedrocanavezi51@gmail.com>
2025-03-18 10:29:32 -0300

    Trabalhando com funções

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+function ola(nome){
+    return `Olá, ${nome}`
+}
+
+console.log(ola("Pedro"))
+
+const soma = function(a, b){
+    return a + b
+}
+
+console.log(soma(1, 2))
+
+const multi = (x, y) => x * y;
+
+console.log(multi(2, 2))
