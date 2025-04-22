5f9b5a94d5123a71af737f6f026dfcb0b2e7ed44
pedro-canavezi <pedrocanavezi51@gmail.com>
2025-04-22 10:09:10 -0300

    Feat: Create get and put functions

--- microsservices-rest/order-service/orderService.js
+++ microsservices-rest/order-service/orderService.js
@@ -17,5 +17,32 @@ app.post('/pedidos', (req, res) => {
     res.send({message: "Pedido recebido com sucesso", orders});
 });
 
+app.get('/pedidos', (req, res) => {
+    res.status(200).json(orders);
+})
 
-app.listen(4000, () => console.log("order service rodando na porta 4000"));
+app.get('pedidos/:id', (req, res) => {
+    const orderId = req.params.id;
+    const order = orders.find(o => o.id === orderId)
+    if(!order){
+        return res.status(404).send({message: "Pedido não encontrado"});
+    }
+    res.status(200).json(order);
+})
+
+app.put('/pedidos/:id', (req, res) => {
+    const orderId = req.params.id;
+    const oderIndex = orders.findIndex(o => o.id === orderId);
+    if(orderIndex === -1){
+        return res.status(404).send({message: "Pedidos não encontrado"});
+    }
+    
+    const {product, quantity} = req.body;
+    if(!product || !quantity){
+        return res.status(400).send({message: "Dados do pedido inválido"});
+    }
+    orders[orderIndex] = {id: orderId, product, quantity};
+    res.status(200).send({message: "Pedido atualizado com sucesso", order: orders[orderIndex]});
+})
+
+app.listen(4000, () => console.log("Order Service rodando na porta 4000"));
