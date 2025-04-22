9584299262668c595cee6e5a4c4cfadc5d3a538f
pedro-canavezi <pedrocanavezi51@gmail.com>
2025-04-22 09:50:55 -0300

    Feat: Create a new order function

--- microsservices-rest/order-service/orderService.js
+++ microsservices-rest/order-service/orderService.js
@@ -3,12 +3,18 @@ const app = express();
 
 app.use(express.json());
 
+let orders = []; //array to store orders
+
 app.post('/pedidos', (req, res) => {
-    const pedidos = req.body;
 
-    console.log({ message: "Pedido recebido com sucesso", pedidos });
-
-        res.send({ message: "Pedido recebido", pedidos });
+    const {id, product, quantity} = req.body;
+    if(!id || !product || !quantity){
+        return res.status(400).send({ message: "Dados do pedido inválido"})
+    }
+    const newOrder = {id, product, quantity};
+    orders.push(newOrder)
+    res.status(201).send({message: "Pedido criado com sucesso", order: newOrder});
+    res.send({message: "Pedido recebido com sucesso", orders});
 });
 
 
